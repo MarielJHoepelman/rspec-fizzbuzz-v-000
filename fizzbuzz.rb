7cf3fcedc95b3fa47381c3d2780d8d01ed9057b6
Mariel Hoepelman <marielhoepelman@gmail.com>
2017-12-25 22:28:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
+  elsif int % 5 == 0
+    "Buzz"
+  else int % 3 && 5 == 0
+    "FizzBuzz"
+
   end
 end
 
